5c4c985f3b119f9dcdad0ba216ef9c36ef45b198
Dharmtej <dharmtej55555@gmail.com>
2024-01-31 18:33:47 +0530

    Bulid a Search Bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import logo from "../assets/logo.webp";
 import { HStack, Image } from "@chakra-ui/react";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SeachInput from "./SeachInput";
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SeachInput />
       <ColorModeSwitch />
     </HStack>
   );
